b99190cb2d129dd65bf4c899bc68a3270adfccc6
TanvirRashid168 <85211878+TanvirRashid168@users.noreply.github.com>
2024-04-25 01:56:04 +0800

    Add files via upload

---
+++ week5/day1/B_Closest_to_the_Left.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,k;
+    cin >> n >> k;
+
+    vector <int> a(n);
+
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+
+    while(k--){
+        int key;
+        cin >> key;
+        int l=0,r=n-1,mid = 0, idx = n;
+
+        while(l<=r){
+            mid = (l+r)/2;
+
+            if(key >= a[mid]){
+                idx = mid;
+                l = mid+1;
+            }else{
+                r = mid-1;
+            }
+        }
+
+        cout << idx+1 << endl;
+
+    }
+
+    return 0;
+}
